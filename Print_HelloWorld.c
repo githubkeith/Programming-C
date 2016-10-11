b6f2436c17fc066c4942e83b46ee535007be3543
githubkeith <apple.keithtcm@yahoo.com.hk>
2016-10-11 22:24:56 +0800

    Programming C Print Hello World

---
+++ Print_HelloWorld.c
@@ -0,0 +1,17 @@
+/*
+
+Author 		 : Keith Tse
+Auth Date	 : 11 Oct 2016
+Program name : Print Text 
+
+*/
+#include <stdio.h>
+void main ()
+{
+	int number = 10 ;
+
+	printf(" Hello World \n");
+
+	printf(" Print the variable number of value = %d \n", number);
+
+}
